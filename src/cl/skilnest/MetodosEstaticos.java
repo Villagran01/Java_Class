1c057bbbf780c170cc80b15ccd7a73d8cad8fc3b
Cristian <cristian.villagran.traslavina@gmail.com>
2025-08-07 19:29:38 -0400

    comienzo de Metodos Estaticos

---
+++ src/cl/skilnest/MetodosEstaticos.java
@@ -0,0 +1,12 @@
+package cl.skilnest;
+
+public class MetodosEstaticos {
+    public static void main(String[] args) {
+
+    }
+    public static int sumar(int numero1, int numero2){
+        int suma = numero1 + numero2;
+        return suma;
+
+    }
+}
